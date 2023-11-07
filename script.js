d4bc4eff773d1dc2a0763de3e6dbbe0edab148fa
jdm-encompass <145727480+jdm-encompass@users.noreply.github.com>
2023-11-07 02:22:10 -0600

    v4.0
    this version just about rdy for production. style and function is fine. just missing the tonal audio files as a choice, instead of an upload

--- script.js
+++ script.js
@@ -1,71 +1,41 @@
-// Select elements from the DOM
-const musicContainer = document.getElementById('music-container');
-const playBtn = document.getElementById('play');
-const audio = document.getElementById('audio');
-const progressContainer = document.getElementById('progress-container');
-const progress = document.getElementById('progress');
-const audioFileInput = document.getElementById('audio-file');
-const delayInput = document.getElementById('delay-input');
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.getElementById('audio');
+    const playBtn = document.getElementById('play-btn');
+    const audioFileInput = document.getElementById('audio-file');
+    const delaySlider = document.getElementById('delay-slider');
+    const sliderValue = document.getElementById('slider-value');
 
-// Play song
-function playSong() {
-  musicContainer.classList.add('play');
-  playBtn.querySelector('svg').innerHTML = '<path d="M5.5 3.5v9h1v-9h-1zm4 0v9h1v-9h-1z"/>'; // Pause icon
-  audio.play();
-}
+    // Update slider value display
+    delaySlider.oninput = function() {
+        sliderValue.textContent = this.value + 's';
+    };
 
-// Pause song
-function pauseSong() {
-  musicContainer.classList.remove('play');
-  playBtn.querySelector('svg').innerHTML = '<path d="M11.596 8.697l-6.363 3.692V4.005l6.363 3.692z"/>'; // Play icon
-  audio.pause();
-}
+    // Play or pause the audio
+    playBtn.onclick = function() {
+        if (audio.paused) {
+            audio.play();
+            // Change icon to pause
+            this.innerHTML = '<svg viewBox="0 0 16 16" class="bi bi-pause-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 3.5v9h1v-9h-1zm4 0v9h1v-9h-1z"/></svg>';
+        } else {
+            audio.pause();
+            // Change icon to play
+            this.innerHTML = '<svg viewBox="0 0 16 16" class="bi bi-play-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M11.596 8.697l-6.363 3.692V4.005l6.363 3.692z"/></svg>';
+        }
+    };
 
-// Update progress bar
-function updateProgress(e) {
-  const { duration, currentTime } = e.srcElement;
-  const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`;
-}
+    // Load audio file
+    audioFileInput.onchange = function() {
+        if (this.files && this.files[0]) {
+            audio.src = URL.createObjectURL(this.files[0]);
+            audio.load();
+            playBtn.disabled = false; // Enable the play button
+        }
+    };
 
-// Set progress bar
-function setProgress(e) {
-  const width = this.clientWidth;
-  const clickX = e.offsetX;
-  const duration = audio.duration;
-  audio.currentTime = (clickX / width) * duration;
-}
-
-// Event listeners
-playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play');
-  if (isPlaying) {
-    pauseSong();
-  } else {
-    playSong();
-  }
-});
-
-// Change song
-audioFileInput.addEventListener('change', function() {
-  const files = audioFileInput.files;
-  if (files.length > 0) {
-    const file = files[0];
-    audio.src = URL.createObjectURL(file);
-    playSong();
-  }
-});
-
-// Time/song update
-audio.addEventListener('timeupdate', updateProgress);
-
-// Click on progress bar
-progressContainer.addEventListener('click', setProgress);
-
-// Song ends
-audio.addEventListener('ended', function() {
-  let delay = parseInt(delayInput.value, 10);
-  setTimeout(function() {
-    playSong();
-  }, delay * 1000);
+    // Loop the audio based on the delay input
+    audio.onended = function() {
+        setTimeout(function() {
+            audio.play();
+        }, parseFloat(delaySlider.value) * 1000);
+    };
 });
